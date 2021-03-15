7663f6247d8dbfd2f9c06b694f37abb6b35c3955
renantescaro <renantescaro@yahoo.com.br>
2021-03-14 22:43:20 -0300

    controle bluetooth

--- inicial.py
+++ inicial.py
@@ -1,5 +1,12 @@
 from classes.patas import Patas
+from classes.joystick import Joystick
 
-patas = Patas()
+# patas    = Patas()
+joystick = Joystick()
 
-patas.frente()
+# patas.frente()
+
+while True:
+    joystick.verificar_comandos()
+    print(joystick.analogico)
+    print(joystick.botao)
